521331f18ddb68877e6b8f76bb291b60ef6c5178
unknown <ddw_github@de-weerd.name>
2020-05-24 20:15:13 +0200

    new fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,14 +12,17 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         // create all Areas
-        $values = array("Village du Pichon Frétillant","Bois de Litneg","Forêt d'Astrub","Bordure de Brâkmar","Cité d'Astrub","Désolation de Sidimote","Cimetière des Torturés","Bordure d'Aerdala","Bordure d'Akwadala","Forêt de Pandala","Bordure de Feudala","Champs de glace","Lac gelé","Forêt des pins perdus","Berceau d'Alma","Larmes d'Ouronigride","Crevasse Perge","Forêt pétrifiée","Crocs de verre","Ruche des Gloursons","Cavernes des Givrefoux","Tourbière nauséabonde","Champ des Ingalsse","Marécages sans fond","Jungle obscure","Salles des Abîmes","Retraite des Éternels","Carrière Aurifère","Creuset des Fortunés","Catacombres","Hauts Ténébreux","Ruelles des Eaux-Suaires","Chemins d'hier","Jour présent","Lendemains incertains","Forêt Sombre","Cirque de Cania","Mont Torrideau","Dents de Pierre","Île de Kartonpath","Salles des Embruns","Tourbière sans fond","Jungle Interdite","Tréfonds des Trithons","Abîme de R'lyugluglu","Vestiges engloutis","Dunes des ossements","Territoire Cacterre","Pyramide Maudite","Route des Roulottes","Hauts des Hurlements","Gisgoul","Nimotopia","Bordure de Terrdala","Caserne du Jour sans fin","Plaines herbeuses","Île du Minotoror","Village de la Canopée","Landes de Cania","Tronc de l'arbre Hakam","Île de Grobe","Caverne des Fungus","Dimension Obscure","Territoire des Porcos","Prairies d'Astrub","Clairière de Brouce Boulgoure","Cimetière","Plaine des Porkass","Îlot de la Couronne","Arche d'Otomaï","Territoire des Dragodindes Sauvages","Chemin du Crâne","Gorge des Vents Hurlants","Forêt enneigée");
-        foreach ($values as &$value) {   
+        $values = array("Village du Pichon Frétillant", "Bois de Litneg", "Forêt d'Astrub", "Bordure de Brâkmar", "Cité d'Astrub", "Désolation de Sidimote", "Cimetière des Torturés", "Bordure d'Aerdala", "Bordure d'Akwadala", "Forêt de Pandala", "Bordure de Feudala", "Champs de glace", "Lac gelé", "Forêt des pins perdus", "Berceau d'Alma", "Larmes d'Ouronigride", "Crevasse Perge", "Forêt pétrifiée", "Crocs de verre", "Ruche des Gloursons", "Cavernes des Givrefoux", "Tourbière nauséabonde", "Champ des Ingalsse", "Marécages sans fond", "Jungle obscure", "Salles des Abîmes", "Retraite des Éternels", "Carrière Aurifère", "Creuset des Fortunés", "Catacombres", "Hauts Ténébreux", "Ruelles des Eaux-Suaires", "Chemins d'hier", "Jour présent", "Lendemains incertains", "Forêt Sombre", "Cirque de Cania", "Mont Torrideau", "Dents de Pierre", "Île de Kartonpath", "Salles des Embruns", "Tourbière sans fond", "Jungle Interdite", "Tréfonds des Trithons", "Abîme de R'lyugluglu", "Vestiges engloutis", "Dunes des ossements", "Territoire Cacterre", "Pyramide Maudite", "Route des Roulottes", "Hauts des Hurlements", "Gisgoul", "Nimotopia", "Bordure de Terrdala", "Caserne du Jour sans fin", "Plaines herbeuses", "Île du Minotoror", "Village de la Canopée", "Landes de Cania", "Tronc de l'arbre Hakam", "Île de Grobe", "Caverne des Fungus", "Dimension Obscure", "Territoire des Porcos", "Prairies d'Astrub", "Clairière de Brouce Boulgoure", "Cimetière", "Plaine des Porkass", "Îlot de la Couronne", "Arche d'Otomaï", "Territoire des Dragodindes Sauvages", "Chemin du Crâne", "Gorge des Vents Hurlants", "Forêt enneigée");
+        foreach ($values as &$value) {
             $area = new Area();
             $area->setName($value);
             $manager->persist($area);
         }
-        $position = array("[5,-19]","[-76,-42]","[-75,-74]","Enutrosor [-1,-1]","Srambad [2,2]","Xelorium [1,6]","[0,0]","[5,-6]","Profondeur de sufokia [23,26]","Bonta [-33,-56] ou Brakmar [-26,36]","Sabloon [15,-57]");
-        foreach ($position as &$position) {   
+
+        $manager->flush();
+
+        $positions = array("[5,-19]", "[-76,-42]", "[-75,-74]", "Enutrosor [-1,-1]", "Srambad [2,2]", "Xelorium [1,6]", "[0,0]", "[5,-6]", "Profondeur de sufokia [23,26]", "Bonta [-33,-56] ou Brakmar [-26,36]", "Sabloon [15,-57]");
+        foreach ($positions as &$value) {
             $position = new Position();
             $position->setName($value);
             $manager->persist($position);
